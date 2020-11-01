20f7cbc0ef547c13730fde444533258352706d2b
Peter Anderson <pete.anderson@gmail.com>
2020-11-01 19:34:05 +0100

    demo seems to work if run via http-server

---
+++ js/fun.js
@@ -0,0 +1,5 @@
+console.log("fun!");
+
+let fun = "FUN!!!!!";
+
+export default fun;
---
+++ js/script.js
@@ -0,0 +1,5 @@
+import fun from "./fun.js";
+
+console.log("fun from the other file...: ", fun);
+
+console.log("script!");
